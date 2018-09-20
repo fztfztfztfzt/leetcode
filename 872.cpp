9d7af2832f0502f563e81725e60d8e4e9c77f930
Polaris Feng <fztfztfztfzt@gmail.com>
2018-09-20 19:28:36 +0800

    18/9/20

---
+++ 872.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        
+    }
+};
+
+class NextLeaf {
+public:
+    void NextLeaf(TreeNode* root) {
+
+    }
+    bool hasNextLeaf() {
+        
+    }
+    int getNextLeaf() {
+
+    }
+private:
+
+}
