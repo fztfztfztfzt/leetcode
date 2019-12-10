e929715497c12b152c5c0dfdc2d99bbb555abc94
Polaris Feng <fztfztfztfzt@gmail.com>
2019-12-10 13:43:57 +0800

    19/12/10

--- 546.cpp
+++ 546.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int removeBoxes(vector<int>& boxes) {
         int n = boxes.size();
-        int dp[100][100][100];
+        int dp[100][100][100]={0};
         return helper(boxes,0,n-1,0,dp);
     }
 private:
     int helper(vector<int>& boxes,int i,int j,int k,int dp[100][100][100]){
         if(j<i) return 0;
-        if(dp[i][j][k]) return dp[i][j][k];
+        if(dp[i][j][k]>0) return dp[i][j][k];
         int res = (1+k)*(1+k)+helper(boxes,i+1,j,0,dp);
         for(int m=i+1;m<=j;m++){
             if(boxes[m]==boxes[i]){
